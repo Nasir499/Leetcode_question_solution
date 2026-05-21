4836c566e1dfa18a4557fe20833360075e2855e6
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-05-21 20:05:33 +0530

    Time: 10 ms (14.92%), Space: 24.8 MB (12.99%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,25 @@
+bool cmp(vector<int> &a,vector<int> &b){
+    return a[1]<b[1];
+}
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),cmp);
+        vector<vector<int>>  ans;
+        int n = intervals.size();
+        ans.push_back(intervals[n-1]);
+        for(int i=n-2;i>=0;i--){
+            vector<int> currIntrvl = intervals[i];
+            if(currIntrvl[1]>=ans[ans.size()-1][0]){
+                //curr.end>=last.start => overlapping
+                ans[ans.size()-1][0]=min(ans[ans.size()-1][0],currIntrvl[0]);
+                // ans[ans.size()-1][1]=min(ans[ans.size()-1][1],currIntrvl[1]);
+            }
+            else{//not overlapping
+                ans.push_back(intervals[i]);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
