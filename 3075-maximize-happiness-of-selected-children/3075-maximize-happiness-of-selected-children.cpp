d30ecfecc6438f36a7c1a4bbe195d7d6eff6b4c3
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-05-23 13:45:33 +0530

    Time: 157 ms (55.05%), Space: 107.8 MB (89.48%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        priority_queue<int> pq;
-        for(auto x : happiness){
-            pq.push(x);
-        }
+        // priority_queue<int> pq;
+        // for(auto x : happiness){
+        //     pq.push(x);
+        // }
+        // long long sum=0;
+        // sum+=pq.top();
+        // pq.pop();
+        // for(int i=1;i<k;i++){
+        //     int x = pq.top()-i;            
+        //     sum += (x>0)?x:0;
+        //     pq.pop();
+        // }
+        // return sum;
+        sort(happiness.begin(),happiness.end(),greater<int>());
         long long sum=0;
-        sum+=pq.top();
-        pq.pop();
-        for(int i=1;i<k;i++){
-            int x = pq.top()-i;
-            
-            sum += (x>0)?x:0;
-            pq.pop();
+        for(int i=0;i<k;i++){
+            sum += max(0,happiness[i]-i);
         }
         return sum;
     }
