5ff2ce252241f10492249b4a7397900acd110c7a
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-05-30 12:01:59 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.15%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<9){
+                nums[i]++;
+                return nums;
+            }
+            nums[i]=0;
+        }
+        nums.insert(nums.begin(),1);
+        return nums;
+    }
+};
