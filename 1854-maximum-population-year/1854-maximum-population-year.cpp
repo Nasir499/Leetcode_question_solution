f0568b23c3fa6f27344ad832e31ecbaf9f3e6a52
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-06-09 17:28:51 +0530

    Time: 0 ms (100%), Space: 11.3 MB (44.64%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        vector<int> diff(2051,0);
-        for(auto &l:logs){
-            int birth=l[0];
-            int death=l[1];
+        //difference array technique
+        // vector<int> diff(2051,0);
+        // for(auto &l:logs){
+        //     int birth=l[0];
+        //     int death=l[1];
 
-            diff[birth]+=1;
-            diff[death]-=1;
+        //     diff[birth]+=1;
+        //     diff[death]-=1;
+        // }
+        // int maxpop=0,curr=0,miny=2050;
+        // for(int i=1950;i<2051;i++){
+        //     curr += diff[i];
+        //    if(curr>maxpop){
+        //     maxpop=curr;
+        //     miny=i;
+        //    }
+        // }
+        // return miny;
+        //line sweep technique;
+        vector<pair<int,int>> events;
+        for(auto &x:logs){
+            events.push_back({x[0],+1});
+            events.push_back({x[1],-1});
         }
-        int maxpop=0,curr=0,miny=2050;
-        for(int i=1950;i<2051;i++){
-            curr += diff[i];
-           if(curr>maxpop){
-            maxpop=curr;
-            miny=i;
-           }
+
+        sort(events.begin(),events.end());
+
+        int currpop=0,maxpop=0,miny=2050;
+
+        for(auto &e:events){
+            currpop += e.second;
+            if(currpop>maxpop){
+                maxpop=currpop;
+                miny=e.first;
+            }
         }
         return miny;
     }
