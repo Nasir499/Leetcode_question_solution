b2d0e188166e464b59d2b7c73d3c75d72c99aae9
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-06-10 01:11:59 +0530

    Time: 12 ms (69.93%), Space: 37 MB (47.72%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size(),count=0;
+        // for(int r=0;r<n;r++){
+        //     for(int c=0;c<n;c++){
+        //         bool isequal=true;
+        //         for(int i=0;i<n;i++){
+        //             if(grid[r][i]!=grid[i][c]){
+        //                 isequal=false;
+        //                 break;
+        //             }
+        //         }
+        //         if(isequal==true) count++;
+        //     }
+        // }
+        // return count;
+        map<vector<int>,int> m;
         for(int r=0;r<n;r++){
-            for(int c=0;c<n;c++){
-                bool isequal=true;
-                for(int i=0;i<n;i++){
-                    if(grid[r][i]!=grid[i][c]){
-                        isequal=false;
-                        break;
-                    }
-                }
-                if(isequal==true) count++;
+            m[grid[r]]++;
+        }
+        for(int c=0;c<n;c++){
+            vector<int> temp;
+            for(int r=0;r<n;r++){
+                temp.push_back(grid[r][c]);
             }
+            count+=m[temp];
         }
         return count;
     }
