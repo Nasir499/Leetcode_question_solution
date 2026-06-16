f923b7b31227267d1ed83b84693f6c312717af39
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-06-16 13:22:15 +0530

    Time: 107 ms (38.62%), Space: 21.9 MB (50.43%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    static bool cmp(string &s1,string &s2){
+        return s1.length()<s2.length();
+    }
+    bool isPredecessor(string &s1,string &s2){
+        int m = s1.length();
+        int n = s2.length();
+        if(m>=n or n-m!=1) return false;
+        int i=0,j=0;
+        while(i<m and j<n){
+            if(s1[i]==s2[j]) i++;
+            j++;
+        }
+        return i==m;
+    }
+    int lis(vector<string>& w,int p,int i){
+        if(i==w.size()) return 0;
+        if(dp[p+1][i]!=-1) return dp[p+1][i];
+        int take=0,skip=0;
+        if(p==-1 or isPredecessor(w[p],w[i])){
+            take = 1+lis(w,i,i+1);
+        }
+        skip = lis(w,p,i+1);
+        return dp[p+1][i]=max(take,skip);
+    }
+    int longestStrChain(vector<string>& words) {
+        memset(dp,-1,sizeof(dp));
+        sort(words.begin(),words.end(),cmp);
+        return lis(words,-1,0);
+    }
+};
