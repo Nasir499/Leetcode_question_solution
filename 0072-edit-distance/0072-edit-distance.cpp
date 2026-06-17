4b14f9e44f94ea784abea2eee4fed58f1d7b08ff
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-06-17 13:05:34 +0530

    Time: 43 ms (12.89%), Space: 11.7 MB (86.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int dp[505][505];
-    int f(string &s1,string &s2,int i,int j){
-        if(i==s1.size()) return s2.size()-j;
-        if(j==s2.size()) return s1.size()-i;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]) return f(s1,s2,i+1,j+1);
-        return dp[i][j]= min({1+f(s1,s2,i+1,j+1) , 1+f(s1,s2,i+1,j),1+f(s1,s2,i,j+1)});
+    int f(string &s1,string &s2,int m,int n){
+       if(m==0 or n==0) return m+n;
+        if(dp[m][n]!=-1) return dp[m][n];
+        if(s1[m-1]==s2[n-1]) return dp[m][n]=f(s1,s2,m-1,n-1);
+        return dp[m][n]= min({1+f(s1,s2,m-1,n-1) , 1+f(s1,s2,m-1,n),1+f(s1,s2,m,n-1)});
     }
     int minDistance(string word1, string word2) {
         memset(dp,-1,sizeof dp);
-        return f(word1,word2,0,0);
+        return f(word1,word2,word1.length(),word2.length());
     }
 };
