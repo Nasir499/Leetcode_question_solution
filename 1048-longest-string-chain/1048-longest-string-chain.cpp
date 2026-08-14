58ed26f7ce277799f0aab86a01872d62964e217f
Nasir <161972376+Nasir499@users.noreply.github.com>
2026-08-14 20:56:12 +0530

    Time: 47 ms (55.24%), Space: 18.8 MB (69.81%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[1001][1001];
+    // int dp[1001][1001];
     static bool cmp(string &s1,string &s2){
         return s1.length()<s2.length();
     }
@@ -15,19 +15,29 @@ public:
         }
         return i==m;
     }
-    int lis(vector<string>& w,int p,int i){
-        if(i==w.size()) return 0;
-        if(dp[p+1][i]!=-1) return dp[p+1][i];
-        int take=0,skip=0;
-        if(p==-1 or isPredecessor(w[p],w[i])){
-            take = 1+lis(w,i,i+1);
-        }
-        skip = lis(w,p,i+1);
-        return dp[p+1][i]=max(take,skip);
-    }
+    // int lis(vector<string>& w,int p,int i){
+    //     if(i==w.size()) return 0;
+    //     if(dp[p+1][i]!=-1) return dp[p+1][i];
+    //     int take=0,skip=0;
+    //     if(p==-1 or isPredecessor(w[p],w[i])){
+    //         take = 1+lis(w,i,i+1);
+    //     }
+    //     skip = lis(w,p,i+1);
+    //     return dp[p+1][i]=max(take,skip);
+    // }
     int longestStrChain(vector<string>& words) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         sort(words.begin(),words.end(),cmp);
-        return lis(words,-1,0);
+        // return lis(words,-1,0);
+         int n = words.size();
+        vector<int> dp(n,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(isPredecessor(words[j],words[i])){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
